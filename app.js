5f619f52627b4014d641726219bd5782f057671b
tle-nopanan <P.nopanan@kkumail.com>
2023-12-05 00:47:42 +0700

    template

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const debug = require("debug")("app");
 const morgan = require("morgan");
+const path = require("path");
 
 const app = express();
 const port = 3000;
 
 app.use(morgan("combined"));
+app.use(express.static(path.join(__dirname, "/public/")));
 
 app.get("/", (req, res) => {
-  res.send("Hello Wolrd");
+  res.send("Hello Wolrd Co., Ltd.");
 });
 
 app.listen(port, () => {
