d9bc29e3b389b09a9e666665c548a3219919ae84
tle-nopanan <P.nopanan@kkumail.com>
2023-12-04 23:46:26 +0700

    Initial Project

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const debug = require("debug")("app");
+const morgan = require("morgan");
+
+const app = express();
+const port = 3000;
+
+app.use(morgan("combined"));
+
+app.get("/", (req, res) => {
+  res.send("Hello Wolrd");
+});
+
+app.listen(port, () => {
+  debug(`Listening on port${port}`);
+});
