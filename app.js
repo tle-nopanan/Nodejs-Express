104c0a4db63faa11e3ab3fe0a086fd026134de96
tle-nopanan <P.nopanan@kkumail.com>
2023-12-31 02:41:06 +0700

    Sending data within webapp

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const debug = require("debug")("app");
 const morgan = require("morgan");
 const path = require("path");
+const products = require("./data/products.json");
 const productRouter = express.Router();
 
 const app = express();
@@ -14,7 +15,7 @@ app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 productRouter.route("/").get((req, res) => {
-  res.send("Hello World !! I'm Product");
+  res.render("products", products);
 });
 
 productRouter.route("/1").get((req, res) => {
