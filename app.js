3cc88150c068fe1d29fed4043c810661efa0cec9
tle-nopanan <P.nopanan@kkumail.com>
2023-12-31 02:01:11 +0700

    Router

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const debug = require("debug")("app");
 const morgan = require("morgan");
 const path = require("path");
+const productRouter = express.Router();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -11,6 +12,16 @@ app.use(express.static(path.join(__dirname, "/public/")));
 
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+
+productRouter.route("/").get((req, res) => {
+  res.send("Hello World !! I'm Product");
+});
+
+productRouter.route("/1").get((req, res) => {
+  res.send("Hello World !! I'm Product 1");
+});
+
+app.use("/products", productRouter);
 
 app.get("/", (req, res) => {
   res.render("index", { username: "Tleza55+", customers: ["Yo", "To", "Bo"] });
