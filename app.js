0170d85071fed27cf64145061ba429db9692d708
tle-nopanan <P.nopanan@kkumail.com>
2023-12-31 03:06:05 +0700

    Manage routes to be more efficient

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ const express = require("express");
 const debug = require("debug")("app");
 const morgan = require("morgan");
 const path = require("path");
-const products = require("./data/products.json");
-const productRouter = express.Router();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+const productsRouter = require("./src/router/productsRouter");
 
 app.use(morgan("combined"));
 app.use(express.static(path.join(__dirname, "/public/")));
@@ -14,15 +13,7 @@ app.use(express.static(path.join(__dirname, "/public/")));
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
-productRouter.route("/").get((req, res) => {
-  res.render("products", products);
-});
-
-productRouter.route("/1").get((req, res) => {
-  res.send("Hello World !! I'm Product 1");
-});
-
-app.use("/products", productRouter);
+app.use("/products", productsRouter);
 
 app.get("/", (req, res) => {
   res.render("index", { username: "Tleza55+", customers: ["Yo", "To", "Bo"] });
